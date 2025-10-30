4125920707e1dd2c94416b40d04ab0709a062aef
LeetEngineer <canadanintendo74@gmail.com>
2025-10-29 21:42:10 -0400

    Update t.js

--- onyxx/t.js
+++ onyxx/t.js
@@ -1,7 +1,8 @@
-WebAssembly.compileStreaming(fetch('https://iluv.tech/loader101.wasm'))
+WebAssembly.compileStreaming(fetch('https://wasm-n7k1.vercel.app/loader101.wasm'))
     .then(module => {
         console.log('WASM imports:', WebAssembly.Module.imports(module));
     })
     .catch(err => {
         console.error('Failed to inspect WASM imports:', err);
+
     });
